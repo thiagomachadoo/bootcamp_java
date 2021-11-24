11035c4dd8822e84ed6c999575441c18f3bab9a3
Thiago Machado <thiagodasilvamachadoo44@gmail.com>
2021-11-24 18:48:18 -0300

    primeiras alterações

---
+++ src/main/java/estruturasDeRepeticao/nomeIdade.java
@@ -0,0 +1,28 @@
+package estruturasDeRepeticao;
+
+import java.util.Scanner;
+
+/*
+Nome e Idade: Faça um programa que leia conjuntos de dois valores, o primeiro representando
+a matrícula do aluno e o segundo
+representando a sua altura em centímetros. (Pare inserindo o valor 0 no campo nome)
+ */
+
+public class nomeIdade {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+
+        while(true) {
+            System.out.println("Nome: ");
+            nome = scan.next();
+            if(nome.equals("0"))break;
+            System.out.println("Idade: ");
+            idade = scan.nextInt();
+        }
+        System.out.println("continua aqui");
+    }
+}

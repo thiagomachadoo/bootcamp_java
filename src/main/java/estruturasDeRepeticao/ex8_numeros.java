445288fcf6f4bf4ab475d1aa19a508b5ca7548b7
Thiago Machado <thiagodasilvamachadoo44@gmail.com>
2021-12-02 12:02:50 -0300

    02/12

---
+++ src/main/java/estruturasDeRepeticao/ex8_numeros.java
@@ -0,0 +1,32 @@
+package estruturasDeRepeticao;
+
+import java.util.Scanner;
+
+/*
+Ler 10 números e imprimir quantos são pares e quantos são ímpares.
+ */
+public class ex8_numeros {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+
+        int numeros;
+        int numerosImpares = 0;
+        int numerosPares = 0;
+        int contador = 0;
+
+
+        for (int i = 0; i < 10; i++){
+            System.out.println("Digite 10 números: ");
+            numeros = leia.nextInt();
+            if(numeros % 2 == 0) {
+                numerosPares++;
+            }else{
+                numerosImpares++;
+            }
+            contador++;
+        }
+        System.out.println("Quantidade de pares: " + numerosPares);
+        System.out.println("Quantidade de ímpares: " + numerosImpares);
+    }
+}

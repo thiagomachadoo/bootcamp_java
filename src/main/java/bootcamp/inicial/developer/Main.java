28a6f4ea6d7f44a9cb923268d324c1e8b93c0c7f
Thiago Machado <thiagodasilvamachadoo44@gmail.com>
2021-12-09 13:55:21 -0300

    09/12 - Objetos e instancias

--- src/main/java/bootcamp/inicial/developer/Main.java
+++ src/main/java/bootcamp/inicial/developer/Main.java
@@ -1,7 +1,22 @@
 package bootcamp.inicial.developer;
 
+import logicaDeProgramacao.ex14_atribuicoesEtestes;
+
 public class Main {
     public static void main(String[] args) {
 
+        int a = 1;
+        int b = 1;
+
+        System.out.println("int A = "+ a + " int B = " + b);
+        a = 2;
+        System.out.println("int A = "+ a + " int B = " + b);
+
+        ex14_atribuicoesEtestes ex14A = new ex14_atribuicoesEtestes(1);
+        ex14_atribuicoesEtestes ex14B = ex14A;
+
+        System.out.println("obj A = "+ a + " obj B = " + b);
+        ex14A.setNum(2);
+        System.out.println("obj A = "+ a + " obj B = " + b);
     }
 }
---
+++ src/main/java/logicaDeProgramacao/ex14_atribuicoesEtestes.java
@@ -0,0 +1,19 @@
+package logicaDeProgramacao;
+
+public class ex14_atribuicoesEtestes {
+
+       Integer num;
+
+    @Override
+    public String toString() {
+        return this.num.toString();
+    }
+
+    public void setNum(Integer num){
+           this.num = num;
+       }
+
+    public ex14_atribuicoesEtestes(Integer num) {
+        this.num = num;
+    }
+}

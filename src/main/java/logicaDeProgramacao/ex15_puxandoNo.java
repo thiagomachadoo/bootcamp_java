0032446bbc6d8d74dc3df3578adc0dc8d159c850
Thiago Machado <thiagodasilvamachadoo44@gmail.com>
2021-12-09 14:45:59 -0300

    09/12 - Refatoração de nó com generics

--- src/main/java/logicaDeProgramacao/ex15_puxandoNo.java
+++ src/main/java/logicaDeProgramacao/ex15_puxandoNo.java
@@ -7,16 +7,16 @@ public class ex15_puxandoNo {
 
         //no1 -> no2 -> no3 -> no4 -> NULL
 
-        No no1 = new No("Conteúdo no1");
+        No<String> no1 = new No<>("Conteúdo no1");
 
-        No no2 = new No("Conteúdo no2");
+        No<String> no2 = new No<>("Conteúdo no2");
         //referencia apontada
         no1.setProximoNo(no2);
 
-        No no3= new No("Conteúdo no3");
+        No<String> no3= new No<>("Conteúdo no3");
         no2.setProximoNo(no3);
 
-        No no4 = new No("Conteúdo no4");
+        No<String> no4 = new No<>("Conteúdo no4");
         no3.setProximoNo(no4);
 
         System.out.println(no1);
